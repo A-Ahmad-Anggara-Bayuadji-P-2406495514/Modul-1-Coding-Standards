77be62460acc7c0afbefb7aa82ee74c9b9f38a14
anggarabayuadji <a.anggara.bayuadji.p@gmail.com>
2026-02-08 16:07:27 +0700

    Add ProductService dand ProductServiceImpl service class

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -1,4 +1,4 @@
-package id.ac.iui.cs.advprog.eshop;
+package id.ac.ui.cs.advprog.eshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -1,6 +1,6 @@
-package id.ac.iui.cs.advprog.eshop.repository;
+package id.ac.ui.cs.advprog.eshop.repository;
 
-import id.ac.iui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
