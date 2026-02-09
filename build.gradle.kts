24ba11aec4e45ae73660bba5c7f4de11135d020c
anggarabayuadji <a.anggara.bayuadji.p@gmail.com>
2026-02-09 20:12:59 +0700

    added ProductTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "id.ac.iui.cs.advprog"
+group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
 description = "eshop"
 
