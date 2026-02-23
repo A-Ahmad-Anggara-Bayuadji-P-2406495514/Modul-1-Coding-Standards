0330def65eb17eb77941826629e42bcb95a05ebb
anggarabayuadji <a.anggara.bayuadji.p@gmail.com>
2026-02-23 12:48:04 +0700

    fixed issues regarding typo and sonarcloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.5.10"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "4.4.1.3373"
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -77,4 +78,12 @@ tasks.test {
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "A-Ahmad-Anggara-Bayuadji-P-2406495514_Modul-1-Coding-Standards")
+        property("sonar.organization", "a-ahmad-anggara-bayuadji-p-2406495514")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
