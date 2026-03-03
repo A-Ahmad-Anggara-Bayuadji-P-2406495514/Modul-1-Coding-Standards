f19c4ece7109b1230d86ee1be2e00e428f39f03f
anggarabayuadji <a.anggara.bayuadji.p@gmail.com>
2026-03-03 11:23:24 +0700

    applied SOLID

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarReadService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarReadService.java
@@ -3,10 +3,7 @@ import id.ac.ui.cs.advprog.eshop.model.Car;
 
 import java.util.List;
 
-public interface CarService {
-    public Car create(Car car);
+public interface CarReadService {
     public List<Car> findAll();
     Car findById(String carId);
-    public void update(String carId, Car car);
-    public void deleteCarById(String carId);
 }
