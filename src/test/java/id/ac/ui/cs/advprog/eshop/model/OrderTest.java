f614043b1857d59d7597cd6f0479b6c8a53d69c5
anggarabayuadji <a.anggara.bayuadji.p@gmail.com>
2026-03-10 10:57:15 +0700

    [RED] Add Order model skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -1,8 +1,12 @@
+package id.ac.ui.cs.advprog.eshop.model;
+import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class OrderTest {
 
